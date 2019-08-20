66d3039a1b7a0e76cd0dfe41e0c0068b9762104f
mark-solo <mark.an.impostor@gmail.com>
2019-08-20 17:40:56 +0300

    blueprint base

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from config import Config
 
+# creating module objects
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
     # module inits
 
-    # blueprint registration
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
     if not app.debug and not app.testing:
         pass
