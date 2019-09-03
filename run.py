4426f9aa30ede2421d92e53e3f0e6728f95e7afe
mark-solo <mark.an.impostor@gmail.com>
2019-09-03 10:56:21 +0300

    fix: pulling .env file on launch

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import os
 from app import create_app
 
+from dotenv import load_dotenv
+dotenv_path = join(dirname(___file__), '.env')
+load_dotenv(dotenv_path)
+
 config_name = os.getenv('FLASK_ENV') or 'production'
 app = create_app(config_name)
