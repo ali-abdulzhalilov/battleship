94a77b05fd9bc06d2d770ee71d4c8e6a8c15cf84
mark-solo <mark.an.impostor@gmail.com>
2019-09-03 11:02:12 +0300

    fix: fixed dotenv loading

--- run.py
+++ run.py
@@ -2,8 +2,7 @@ import os
 from app import create_app
 
 from dotenv import load_dotenv
-dotenv_path = join(dirname(___file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv()
 
 config_name = os.getenv('FLASK_ENV') or 'production'
 app = create_app(config_name)
